3729ee280de775c48372ba7e11580b5e36a1a093
Mansvi777 <149859692+Mansvi777@users.noreply.github.com>
2025-11-07 23:24:34 +0530

    Update addPost.jsx

--- Evkarya/src/admin/components/addPost.jsx
+++ Evkarya/src/admin/components/addPost.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 
 const AddPost = () => {
-  const url=https://evkarya-backend-iwsx.onrender.com;
+  const url="https://evkarya-backend-iwsx.onrender.com";
   const [title, setTitle] = useState("");
   const [imageUrl, setImageUrl] = useState("");
   const [description, setDescription] = useState("");
@@ -52,7 +52,7 @@ const AddPost = () => {
 
     try {
       const response = await axios.post(
-        "url/api/posts/", // Ensure this URL is correct
+        "${url}/api/posts/", // Ensure this URL is correct
         newPostData,
         {
           headers: {
