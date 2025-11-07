19646cc8243cb4cbecc5b0e0306e9feaa1996f00
Mansvi777 <149859692+Mansvi777@users.noreply.github.com>
2025-11-07 16:11:49 +0530

    Update HomePage.jsx

--- Evkarya/src/customer/pages/HomePage/HomePage.jsx
+++ Evkarya/src/customer/pages/HomePage/HomePage.jsx
@@ -5,6 +5,7 @@ import Footer from '../../components/Footer/Footer';
 import axios from 'axios';
 
 const HomePage = () => {
+    const url=https://evkarya-backend-iwsx.onrender.com;
     // State to store data for each category
     const [bridalWear, setBridalWear] = useState([]);
     const [jewelry, setJewelry] = useState([]);
@@ -16,7 +17,7 @@ const HomePage = () => {
             try {
                 const encodedCategory = encodeURIComponent(category);
                 const res = await axios.get(
-                    `http://localhost:5002/api/posts?category=${encodedCategory}&limit=10`
+                    `url/api/posts?category=${encodedCategory}&limit=10`
                 );
                 setter(res.data);
             } catch (err) {
